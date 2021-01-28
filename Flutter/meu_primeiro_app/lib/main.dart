20b1582a35d6696b64124607e8ae1ff887436cfd
Lucas Limoni <lucas@maidot.pt>
2021-01-28 12:09:51 -0300

    adicionado pagina para buscar imagens com integração ao pexel

--- Flutter/meu_primeiro_app/lib/main.dart
+++ Flutter/meu_primeiro_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'src/home/index.dart' as home;
+import 'src/image_search/index.dart' as search;
 
 String titulo = 'Contador';
 void main() {
@@ -14,19 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.deepPurple,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyStatefulWidget(),
@@ -52,7 +41,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       style: optionStyle,
     ),
     Text(
-      'Index 1: Business',
+      'Index 1: Search Image',
       style: optionStyle,
     ),
     Text(
@@ -63,11 +52,16 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   metodoSwitch(key) {
     debugPrint('Key: $key');
-    key == 0 ? titulo = 'Contador' : titulo = "tab $key";
     switch (key) {
       case 0:
         debugPrint('titulo: $titulo');
+        titulo = 'Contador';
         return home.MyHomePage(title: titulo);
+        break;
+      case 1:
+        debugPrint('titulo: $titulo');
+        titulo = 'Buscador Imagens';
+        return search.MyHomePage(title: titulo);
         break;
       default:
         return _widgetOptions.elementAt(key);
@@ -98,8 +92,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             title: Text('Home'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Business'),
+            icon: Icon(Icons.image_search),
+            title: Text('Search Images'),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.school),
---
+++ Flutter/meu_primeiro_app/lib/src/image_search/index.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+import 'dart:convert';
+import "dart:io";
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+// export '../home/index.dart';
+
+// import pexels
+import "package:pexels/pexels.dart" as pexels;
+export 'index.dart';
+
+// create client instance
+var client = pexels.PexelsClient(
+    '563492ad6f917000010000010eafc146e5e74d0a980cbadf8314b3ca');
+
+var link = "";
+
+Future<Album> fetchAlbum() async {
+// get a random photo
+  var p = await client.getPhoto();
+  link = p.get(pexels.ImageFormats.portrait);
+  stdout.write(link);
+
+  var photoSearch =
+      await client.searchPhotos("Nature", page: 0, resultsPerPage: 1);
+  var link2 = photoSearch[0];
+  var photo = link2.get(pexels.ImageFormats.portrait);
+  stdout.write(photo);
+
+  final result = jsonEncode({"rand": link, "search": photo});
+
+  if (link != null) {
+    return Album.fromJson(jsonDecode(result));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final String photoLinkRandom;
+  final String photoLinkSearch;
+
+  Album({this.photoLinkSearch, this.photoLinkRandom});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+        photoLinkRandom: json['photoLinkRandom'],
+        photoLinkSearch: json['photoLinkSearch']);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, AsyncSnapshot<Album> snapshot) {
+            if (snapshot.hasData) {
+              return new Scaffold(
+                body: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: new Image.network(
+                    link,
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner.
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
